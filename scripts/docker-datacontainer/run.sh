61f037fc4a9b604887cd7225d38bf5e57857080b
Sanjai Ganesh <sanjaiga@microsoft.com>
2020-06-10 12:30:21 -0700

    added dockerfile and script

---
+++ scripts/docker-datacontainer/run.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+
+mkdir -p /usr/data/openapispecs/.git
+cp -R /usr/data/specrepo/* /usr/data/openapispecs/
+cp -R /usr/data/specrepo/.git/* /usr/data/openapispecs/.git/
+
+while true; do timestamp=`date +%Y/%m/%d-%H:%M:%S`; echo "Pulling specs at $timestamp";git pull; sleep 10; done
