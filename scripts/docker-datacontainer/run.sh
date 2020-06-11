4e6a5c33a9fb6ccc1b0f9709b32047b7237a12f5
Sanjai Ganesh Jeenagiri <sanjaiganesh@users.noreply.github.com>
2020-06-10 19:26:37 -0700

    just copy files. setting for init container.

--- scripts/docker-datacontainer/run.sh
+++ scripts/docker-datacontainer/run.sh
@@ -6,12 +6,3 @@ echo "[$timestamp] Copying files to /usr/data/openapispecs"
 mkdir -p /usr/data/openapispecs/.git
 cp -R /usr/data/specrepo/* /usr/data/openapispecs/
 cp -R /usr/data/specrepo/.git/* /usr/data/openapispecs/.git/
-
-timestamp=`date +%Y/%m/%d-%H:%M:%S`
-echo "[$timestamp] Deleting directory /usr/data/specrepo"
-rm -R /usr/data/specrepo/
-
-timestamp=`date +%Y/%m/%d-%H:%M:%S`
-echo "[$timestamp] switching to /usr/data/openapispecs"
-cd /usr/data/openapispecs/
-while true; do timestamp=`date +%Y/%m/%d-%H:%M:%S`; echo "[$timestamp] Checking for updates (git pull).";git pull; sleep 10; done
