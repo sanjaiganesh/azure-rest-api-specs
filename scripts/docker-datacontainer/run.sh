eda2ea861d0e9693f44071f73c0c218110d19cad
Sanjai Ganesh Jeenagiri <sanjaiganesh@users.noreply.github.com>
2020-06-10 18:53:18 -0700

    Update run.sh

--- scripts/docker-datacontainer/run.sh
+++ scripts/docker-datacontainer/run.sh
@@ -1,8 +1,14 @@
 
 #!/bin/bash
 
+echo "[$timestamp] Copying files to /usr/data/openapispecs"
 mkdir -p /usr/data/openapispecs/.git
 cp -R /usr/data/specrepo/* /usr/data/openapispecs/
 cp -R /usr/data/specrepo/.git/* /usr/data/openapispecs/.git/
 
-while true; do timestamp=`date +%Y/%m/%d-%H:%M:%S`; echo "Pulling specs at $timestamp";git pull; sleep 10; done
+echo "[$timestamp] Deleting directory /usr/data/specrepo"
+rm -R /usr/data/specrepo/
+
+echo "[$timestamp] switching to /usr/data/openapispecs"
+cd /usr/data/openapispecs/
+while true; do timestamp=`date +%Y/%m/%d-%H:%M:%S`; echo "[$timestamp] Checking for updates (git pull).";git pull; sleep 10; done
