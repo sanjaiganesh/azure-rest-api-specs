93dfafb32e3aa799c9845026d874267f3706e4a1
Sanjai Ganesh Jeenagiri <sanjaiganesh@users.noreply.github.com>
2020-06-10 23:00:46 -0700

    echo "[$timestamp] Completed. Exiting successfully."

--- scripts/docker-datacontainer/run.sh
+++ scripts/docker-datacontainer/run.sh
@@ -6,3 +6,5 @@ echo "[$timestamp] Copying files to /usr/data/openapispecs"
 mkdir -p /usr/data/openapispecs/.git
 cp -R /usr/data/specrepo/* /usr/data/openapispecs/
 cp -R /usr/data/specrepo/.git/* /usr/data/openapispecs/.git/
+timestamp=`date +%Y/%m/%d-%H:%M:%S`
+echo "[$timestamp] Completed. Exiting successfully."
