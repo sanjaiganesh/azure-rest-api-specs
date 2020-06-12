2b26be78f390bb8c52b8027973a5d51fd45dfde7
Sanjai Ganesh Jeenagiri <sanjaiganesh@users.noreply.github.com>
2020-06-11 19:29:10 -0700

    Only echo in run.sh

--- scripts/docker-datacontainer/run.sh
+++ scripts/docker-datacontainer/run.sh
@@ -2,9 +2,4 @@
 #!/bin/bash
 
 timestamp=`date +%Y/%m/%d-%H:%M:%S`
-echo "[$timestamp] Copying files to /usr/data/openapispecs"
-mkdir -p /usr/data/openapispecs/.git
-cp -R /usr/data/specrepo/* /usr/data/openapispecs/
-cp -R /usr/data/specrepo/.git/* /usr/data/openapispecs/.git/
-timestamp=`date +%Y/%m/%d-%H:%M:%S`
-echo "[$timestamp] Completed. Exiting successfully."
+echo "[$timestamp] OpenAPI spec repo data container repo initial."
