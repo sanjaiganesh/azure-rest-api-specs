20656f030cce710361020785008143dea05cb401
Sanjai Ganesh Jeenagiri <sanjaiganesh@users.noreply.github.com>
2020-06-11 20:14:35 -0700

    Update run.sh

--- scripts/docker-datacontainer/run.sh
+++ scripts/docker-datacontainer/run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 timestamp=`date +%Y/%m/%d-%H:%M:%S`
-echo "[$timestamp] Moving files to /usr/data/openapispecs"
-mv /usr/data/specrepo/* /usr/data/openapispecs/
+echo "[$timestamp] Copying files to /usr/data/openapispecs"
+mkdir -p /usr/data/openapispecs/.git
+cp -R /usr/data/specrepo/* /usr/data/openapispecs/
+cp -R /usr/data/specrepo/.git/* /usr/data/openapispecs/.git/
 
 timestamp=`date +%Y/%m/%d-%H:%M:%S`
 echo "[$timestamp] Deleting directory /usr/data/specrepo"
 rm -R /usr/data/specrepo/
 
-echo "[$timestamp] Init completed successfully."
+timestamp=`date +%Y/%m/%d-%H:%M:%S`
+echo "[$timestamp] Init complete"
