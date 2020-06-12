54829cbf8fdc3caa2a781d6a5bd14e49bf735d8a
Sanjai Ganesh Jeenagiri <sanjaiganesh@users.noreply.github.com>
2020-06-11 20:06:49 -0700

    fixing mv (remove -R)

--- scripts/docker-datacontainer/run.sh
+++ scripts/docker-datacontainer/run.sh
@@ -2,7 +2,7 @@
 
 timestamp=`date +%Y/%m/%d-%H:%M:%S`
 echo "[$timestamp] Moving files to /usr/data/openapispecs"
-mv -R /usr/data/specrepo/* /usr/data/openapispecs/
+mv /usr/data/specrepo/* /usr/data/openapispecs/
 
 timestamp=`date +%Y/%m/%d-%H:%M:%S`
 echo "[$timestamp] Deleting directory /usr/data/specrepo"
