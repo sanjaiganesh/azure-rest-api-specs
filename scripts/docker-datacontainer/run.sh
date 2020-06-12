0ffacc2565701ba7ca85a0249202fc6703fe85ae
Sanjai Ganesh Jeenagiri <sanjaiganesh@users.noreply.github.com>
2020-06-11 19:56:42 -0700

    renaming the folders

--- scripts/docker-datacontainer/run.sh
+++ scripts/docker-datacontainer/run.sh
@@ -1,5 +1,16 @@
-
 #!/bin/bash
 
 timestamp=`date +%Y/%m/%d-%H:%M:%S`
-echo "[$timestamp] OpenAPI spec repo data container repo initial."
+echo "[$timestamp] rename mounted /usr/data/openapispecs to /usr/data/deleteme"
+mv /usr/data/openapispecs /usr/data/deleteme
+
+timestamp=`date +%Y/%m/%d-%H:%M:%S`
+echo "[$timestamp] rename mounted /usr/data/specrepo to /usr/data/openapispecs"
+mv /usr/data/specrepo /usr/data/openapispecs
+
+timestamp=`date +%Y/%m/%d-%H:%M:%S`
+echo "[$timestamp] Deleting directory /usr/data/deleteme"
+rm -R /usr/data/deleteme/
+
+timestamp=`date +%Y/%m/%d-%H:%M:%S`
+echo "[$timestamp] Initialization completed successfully."
