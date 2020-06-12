3b8ed672a7967ba904c08b22225ad713b5a57bda
Sanjai Ganesh Jeenagiri <sanjaiganesh@users.noreply.github.com>
2020-06-11 21:14:44 -0700

    restore to other branch

--- scripts/docker-datacontainer/run.sh
+++ scripts/docker-datacontainer/run.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 timestamp=`date +%Y/%m/%d-%H:%M:%S`
@@ -11,4 +12,6 @@ echo "[$timestamp] Deleting directory /usr/data/specrepo"
 rm -R /usr/data/specrepo/
 
 timestamp=`date +%Y/%m/%d-%H:%M:%S`
-echo "[$timestamp] Init complete"
+echo "[$timestamp] switching to /usr/data/openapispecs"
+cd /usr/data/openapispecs/
+while true; do timestamp=`date +%Y/%m/%d-%H:%M:%S`; echo "[$timestamp] Checking for updates (git pull).";git pull; sleep 10; done
