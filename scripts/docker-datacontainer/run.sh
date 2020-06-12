d17b910105c480084cd561953c1b27ed93cce5af
Sanjai Ganesh Jeenagiri <sanjaiganesh@users.noreply.github.com>
2020-06-11 20:06:24 -0700

    moving files instead

--- scripts/docker-datacontainer/run.sh
+++ scripts/docker-datacontainer/run.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
 timestamp=`date +%Y/%m/%d-%H:%M:%S`
-echo "[$timestamp] rename mounted /usr/data/openapispecs to /usr/data/deleteme"
-mv /usr/data/openapispecs /usr/data/deleteme
+echo "[$timestamp] Moving files to /usr/data/openapispecs"
+mv -R /usr/data/specrepo/* /usr/data/openapispecs/
 
 timestamp=`date +%Y/%m/%d-%H:%M:%S`
-echo "[$timestamp] rename mounted /usr/data/specrepo to /usr/data/openapispecs"
-mv /usr/data/specrepo /usr/data/openapispecs
+echo "[$timestamp] Deleting directory /usr/data/specrepo"
+rm -R /usr/data/specrepo/
 
-timestamp=`date +%Y/%m/%d-%H:%M:%S`
-echo "[$timestamp] Deleting directory /usr/data/deleteme"
-rm -R /usr/data/deleteme/
-
-timestamp=`date +%Y/%m/%d-%H:%M:%S`
-echo "[$timestamp] Initialization completed successfully."
+echo "[$timestamp] Init complete."
